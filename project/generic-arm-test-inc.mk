133ce2516bd7b654e5f96b95171214e4e4fb6fcd
Arve Hjønnevåg <arve@android.com>
2018-09-04 16:57:44 -0700

    [project][generic-arm64] Add timer test
    Bug: 79993976
    Change-Id: I8cfb9983982d8a46ff2ce9c2ca94830d1667d615

--- project/generic-arm-test-inc.mk
+++ project/generic-arm-test-inc.mk
@@ -21,4 +21,5 @@ TRUSTY_ALL_USER_TASKS += \
 	trusty/user/app/sample/ipc-unittest/main \
 	trusty/user/app/sample/ipc-unittest/srv \
 	trusty/user/app/sample/storage-unittest \
+	trusty/user/app/sample/timer \
 
