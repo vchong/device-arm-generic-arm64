0da7ce4d7ffe032133b21410af9b3c5aa7976f30
Yu Shan <shanyu@google.com>
2018-09-21 13:19:14 -0700

    Add keymaster device unittests.
    Change-Id: I30bfcc9cccdb347dcdb2f9a34d350fd58b548165
    Tests: None
    Bug: None

--- project/generic-arm-test-inc.mk
+++ project/generic-arm-test-inc.mk
@@ -16,6 +16,7 @@
 include project/generic-arm-inc.mk
 
 TRUSTY_ALL_USER_TASKS += \
+	trusty/user/app/keymaster/device_unittest \
 	trusty/user/app/sample/app-mgmt-test/srv1 \
 	trusty/user/app/sample/app-mgmt-test/srv2 \
 	trusty/user/app/sample/ipc-unittest/main \
