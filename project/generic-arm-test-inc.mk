7cbd049823f4a4558038f02a98173905d4bcb799
Arve Hjønnevåg <arve@android.com>
2019-04-10 13:14:44 -0700

    [project][generic-arm64] Get list of usertests from trusty/user/base
    Bug: 130301794
    Change-Id: Idcdf09b7baae1d9f618e26bf52b2490552cbb8a8

--- project/generic-arm-test-inc.mk
+++ project/generic-arm-test-inc.mk
@@ -15,27 +15,6 @@
 
 include project/generic-arm-inc.mk
 include trusty/kernel/kerneltests-inc.mk
+include trusty/user/base/usertests-inc.mk
 
 TEST_BUILD := true
-
-TRUSTY_BUILTIN_USER_TASKS += \
-	trusty/user/app/keymaster/device_unittest \
-	trusty/user/app/sample/app-mgmt-test/boot-start-srv \
-	trusty/user/app/sample/app-mgmt-test/client\
-	trusty/user/app/sample/app-mgmt-test/never-start-srv \
-	trusty/user/app/sample/app-mgmt-test/port-start-srv \
-	trusty/user/app/sample/app-mgmt-test/restart-srv \
-	trusty/user/app/sample/app-mgmt-test/syscalls-test/aux \
-	trusty/user/app/sample/app-mgmt-test/syscalls-test/builtin \
-	trusty/user/app/sample/app-mgmt-test/syscalls-test/main \
-	trusty/user/app/sample/ipc-unittest/main \
-	trusty/user/app/sample/ipc-unittest/srv \
-	trusty/user/app/sample/libc-test \
-	trusty/user/app/sample/storage-unittest \
-	trusty/user/app/sample/timer \
-
-TRUSTY_LOADABLE_USER_TASKS += \
-	trusty/user/app/sample/app-mgmt-test/syscalls-test/main/test_apps/duplicate_port \
-	trusty/user/app/sample/app-mgmt-test/syscalls-test/main/test_apps/load_start \
-	trusty/user/app/sample/app-mgmt-test/syscalls-test/main/test_apps/port_start \
-
